a1df89b4380dd389e76be129c2c7dea2879df6ba
MelcherMate <melchermate28@gmail.com>
2024-11-03 10:15:31 +0100

    pdf language select option under development

--- client/src/components/Book/index.tsx
+++ client/src/components/Book/index.tsx
@@ -1,30 +1,43 @@
+import { useState } from "react";
 import "./Book.css";
 
 function Book() {
-  const handleDownload = () => {
-    // Trigger the PDF download
+  const [showOptions, setShowOptions] = useState(false);
+
+  const handleDownload = (lang) => {
     const link = document.createElement("a");
-    link.href = "../../utils/resume/Resume.pdf";
-    link.download = "Resume_Mate_Melcher.pdf";
+    link.href =
+      lang === "HU"
+        ? "../../utils/resume/ResumeHU.pdf"
+        : "../../utils/resume/ResumeENG.pdf";
+    link.download = `Resume_Mate_Melcher_${lang}.pdf`;
     link.click();
   };
 
   return (
-    <>
-      <div className="book">
-        <div className="cover">
-          <p>Hover to open</p>
-          <h2>Resume</h2>
-          <p>Mate Melcher</p>
-        </div>
-        <div className="content">
-          <img src="../../utils/img/cv.png" className="resumeIcon" />
-          <button className="download-button" onClick={handleDownload}>
-            Download
-          </button>
+    <div className="book">
+      <div className="cover">
+        <p>Hover to open</p>
+        <h2>Resume</h2>
+        <p>Mate Melcher</p>
+      </div>
+      <div className="content">
+        <img src="/utils/img/cv.png" className="resumeIcon" alt="Resume Icon" />
+        <div
+          className="download-button"
+          onMouseEnter={() => setShowOptions(true)}
+          onMouseLeave={() => setShowOptions(false)}
+        >
+          <span>Download</span>
+          {showOptions && (
+            <div className="download-options">
+              <button onClick={() => handleDownload("HU")}>HU</button>
+              <button onClick={() => handleDownload("ENG")}>ENG</button>
+            </div>
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
