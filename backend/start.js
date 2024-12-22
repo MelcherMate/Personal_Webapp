ec81edfe296bdb667184642fda342f2089267ff6
MelcherMate <melchermate28@gmail.com>
2024-12-22 13:02:32 +0100

    new start js

--- backend/start.js
+++ backend/start.js
@@ -5,21 +5,30 @@ import app from "./express";
 // # DotEnv configuration
 dotenv.config({ path: path.resolve(__dirname + "/.env") });
 
+console.log("*********************************************************");
+console.log(
+  "**------------- PROCESS ENV:",
+  process.env.NODE_ENV,
+  "-------------**"
+);
+
 // # Server Start
 const actualPort = process.env.PORT || 10000;
+
 app.listen(actualPort, (err) => {
   if (err) {
-    console.log(
-      "**------------- APP ERROR:",
+    console.error(
+      "**------------- SERVER ERROR:",
       err,
-      "(thus stopping the APP)-------------**"
+      "(thus stopping the server)-------------**"
     );
-    process.exit();
+    process.exit(1); // Exit with error
   }
 
   console.log(
-    "**------------- APP STARTED ON PORT:",
+    "**------------- SERVER STARTED ON PORT:",
     actualPort,
     "----------**"
   );
+  console.log("*********************************************************");
 });
