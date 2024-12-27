656b6a6ba4e673d58d0ec31546abf5c40c5fab52
MelcherMate <melchermate28@gmail.com>
2024-12-27 21:36:01 +0100

    new ui... first section is kinda done, picture of me is still needed. links are working, but the logos are missing.

--- client/src/pages/Portfolio/index.tsx
+++ client/src/pages/Portfolio/index.tsx
@@ -1,3 +1,4 @@
+import "../../utils/img/Schoolar.png";
 import "./Portfolio.css";
 
 function Portfolio() {
@@ -8,10 +9,54 @@ function Portfolio() {
         <main>
           <div className="content">
             <div className="section" id="section1">
+              <h2 id="hi">Hi I am</h2>
               <h1 id="myName">Mate Melcher</h1>
+              <div className="separation"> </div>
+              <h3 className="text">
+                {" "}
+                I started out wanting to cure diseases. Then I wanted to sell
+                things. Now I just want my code working. I'm a biochemical
+                engineer/marketing specialist turned web developer, currently
+                working for a bank.{" "}
+              </h3>
+            </div>
+            <div className="linkContainer">
+              <a
+                href="https://www.linkedin.com/in/mate-melcher-5a16601bb/"
+                className="linkButton"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+              <a
+                href="https://scholar.google.com/citations?user=udUgb-IAAAAJ&hl=hu&authuser=1"
+                className="linkButton"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Google Scholar
+              </a>
+              <a
+                href="https://github.com/MelcherMate"
+                className="linkButton"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+              <a
+                href="/utils/resume/ResumeENG.pdf"
+                className="linkButton"
+                download
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Resume
+              </a>
             </div>
             <div className="section" id="section2"></div>
-            <div className="section" id="section3"></div>
+            {/* <div className="section" id="section3"></div> */}
           </div>
         </main>
         <aside className="asideRight"></aside>
