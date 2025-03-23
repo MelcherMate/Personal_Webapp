9def0bcd409b667ce3c832d308ae4a702cb69e72
MelcherMate <melchermate28@gmail.com>
2025-03-23 22:08:56 +0100

    header is close to finish, sitll some work is needed for the active background

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -1,12 +1,21 @@
+import { faPowerOff } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useRef, useState } from "react";
-import "./NavBar.css"; // Feltételezve, hogy a CSS fájl neve Navigation.css
+import "./NavBar.css";
 
 const Navbar = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const activeBgRef = useRef(null);
   const buttonRefs = useRef([]);
+  const [language, setLanguage] = useState("HU");
 
-  const navigationItems = [
+  const navigationItems1 = [
+    "ajánlatok",
+    "aláírandók",
+    "üzenetek",
+    "beállítások",
+  ];
+  const navigationItems2 = [
     "tranzakciók",
     "kártyák, számlák",
     "megtakarítások",
@@ -24,9 +33,7 @@ const Navbar = () => {
 
         activeBg.style.left = `${rect.left - rect.height / 2}px`;
         activeBg.style.width = `${rect.width + rect.height}px`;
-        activeBg.style.top = `${
-          rect.top + (rect.height - activeBg.offsetHeight) / 2
-        }px`;
+        activeBg.style.top = `${rect.height - activeBg.offsetHeight + 30}px`;
       }
     };
 
@@ -47,21 +54,64 @@ const Navbar = () => {
     setActiveIndex(index);
   };
 
+  const handleLanguageToggle = () => {
+    setLanguage(language === "HU" ? "EN" : "HU");
+  };
+
   return (
-    <header className="wrapper">
-      {navigationItems.map((item, index) => (
-        <div
-          key={index}
-          className={`naviButton ${index === activeIndex ? "active" : ""}`}
-          data-index={index}
-          onClick={() => handleButtonClick(index)}
-          ref={(el) => (buttonRefs.current[index] = el)}
-        >
-          {item}
+    <>
+      <header>
+        <div className="mainLogo">
+          <p className="mainLogoText">K&H</p>
         </div>
-      ))}
-      <div className="active-bg" ref={activeBgRef}></div>
-    </header>
+        <div className="wrapper1">
+          <div className="container1">
+            <p className="title">lakossági e-bank</p>
+            <div className="dropdown">
+              <select className="dropdown-select">
+                <option value="melcher">Melcher Máté</option>
+                <option value="bioversee">Bioversee Kft.</option>
+                <option value="kh">K&H Bank Zrt.</option>
+              </select>
+            </div>
+            <div className="naviContainer1">
+              {navigationItems1.map((item, index) => (
+                <div className="naviButton1" data-index={index}>
+                  {item}
+                </div>
+              ))}
+            </div>
+            <input type="text" className="search" placeholder="Keresés..." />
+            <button className="language" onClick={handleLanguageToggle}>
+              {language}
+            </button>
+          </div>
+        </div>
+        <div className="wrapper2">
+          <div className="container2">
+            <div className="naviContainer2">
+              {navigationItems2.map((item, index) => (
+                <div
+                  key={index}
+                  className={`naviButton2 ${
+                    index === activeIndex ? "active" : ""
+                  }`}
+                  data-index={index}
+                  onClick={() => handleButtonClick(index)}
+                  ref={(el) => (buttonRefs.current[index] = el)}
+                >
+                  {item}
+                </div>
+              ))}
+              <div className="active-bg" ref={activeBgRef}></div>
+            </div>
+            <div className="logoutButton">
+              <FontAwesomeIcon icon={faPowerOff} size="lg" />
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
   );
 };
 
