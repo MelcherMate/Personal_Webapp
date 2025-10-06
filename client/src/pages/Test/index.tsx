3a0cbb05d3c0485d44bddbd452b89c585494c55f
MelcherMate <melchermate28@gmail.com>
2025-10-06 18:34:07 +0200

    cards created

--- client/src/pages/Test/index.tsx
+++ client/src/pages/Test/index.tsx
@@ -7,6 +7,11 @@ function Testsite() {
       <aside className="side left"></aside>
       <main className="mainContent">
         <Menu />
+        <div className="metroCards">
+          <div className="metroCard card1">Card 1</div>
+          <div className="metroCard card2">Card 2</div>
+          <div className="metroCard card3">Card 3</div>
+        </div>
       </main>
       <aside className="side right"></aside>
     </div>
